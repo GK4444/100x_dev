85a925df9f48db825bf98b0f8a7e24e4ddea2914
Gaurav Kavhar <gauravkavhar304@gmail.com>
2026-03-19 22:42:41 +0530

    static function in class

--- week-1/week1_3.js
+++ week-1/week1_3.js
@@ -5,6 +5,9 @@ class Player{
         this.sport = sport
         this.rank = rank
     }
+    static myJob() {
+        console.log(`I'm a Player.`)
+    }
 
     info() {
         console.log(`${this.name} plays ${this.sport} and is ranked #${this.rank}`)
@@ -15,3 +18,4 @@ const p1 = new Player('Kohli', 'cricket', '1')
 const p2 = new Player('Messi', 'football', '1')
 p1.info()
 p2.info()
+Player.myJob()
