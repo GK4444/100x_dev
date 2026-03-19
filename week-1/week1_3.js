43516b67ad53ab6982d553ac1e8e32293d4296c6
Gaurav Kavhar <gauravkavhar304@gmail.com>
2026-03-19 22:39:52 +0530

    classes and objects

---
+++ week-1/week1_3.js
@@ -0,0 +1,17 @@
+// Classes, objects
+class Player{
+    constructor (name, sport, rank){
+        this.name = name
+        this.sport = sport
+        this.rank = rank
+    }
+
+    info() {
+        console.log(`${this.name} plays ${this.sport} and is ranked #${this.rank}`)
+    }
+}
+
+const p1 = new Player('Kohli', 'cricket', '1')
+const p2 = new Player('Messi', 'football', '1')
+p1.info()
+p2.info()
