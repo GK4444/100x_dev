8cc73d32833ed28632849806f8a5c7bd7e21f615
Gaurav Kavhar <gauravkavhar304@gmail.com>
2026-03-20 21:42:07 +0530

    json and objects

--- week-1/week1_3.js
+++ week-1/week1_3.js
@@ -1,4 +1,5 @@
-// Classes, objects
+// Classes
+console.log("1.Classes")
 class Player{
     constructor (name, sport, rank){
         this.name = name
@@ -19,3 +20,32 @@ const p2 = new Player('Messi', 'football', '1')
 p1.info()
 p2.info()
 Player.myJob()
+
+
+// JSON
+console.log("2.JSON")
+const user = {
+    "user": "gk",
+    "gender": "male"
+}
+const userString = JSON.stringify(user)
+console.log(userString)
+const parserdUser = JSON.parse(userString)
+console.log(parserdUser)
+
+// Objects (Dicts)
+let obj = {
+    "key1": "value1",
+    "key2": "value2",
+}
+console.log(`obj: ${obj}`)
+let keys = Object.keys(obj)
+console.log(`keys: ${keys}`)
+let values = Object.values(obj)
+console.log(`values: ${values}`)
+let hasProperty = Object.hasOwn(obj, "property")
+console.log(`hasProperty: ${hasProperty}`)
+obj = Object.assign({}, obj, {"property": false})
+hasProperty = Object.hasOwn(obj, "property")
+console.log(`After assign hasProperty: ${hasProperty}`)
+console.log(`final obj: ${Object.entries(obj)}`)
